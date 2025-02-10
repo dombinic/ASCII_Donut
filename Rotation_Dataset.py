638a724990e564e02180e62fc70bc50e9b04a67a
dombinic <dominiclpeters@gmail.com>
2025-02-10 17:54:50 -0600

    Update Rotation_Dataset.py

--- Rotation_Dataset.py
+++ Rotation_Dataset.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Ranges
 speed = np.linspace(0.5, 3.0, 10)
-lum = np.lib(1, 5, 10)
+lum = np.linspace(1, 5, 10)
 
 # All combos of speed and luminance
 speed_in, lum_in = np.meshgrid(speed, lum)
@@ -14,13 +14,13 @@ lum_in = lum_in.flatten()
 A_step = 0.03 + 0.01 * speed_in - 0.002 * lum_in
 B_step = 0.02 + 0.008 * lum_in + 0.005 * speed_in
 
-data = {'Speed Input': speed_in,
-        'Luminance Input': lum_in,
-        'A-step': A_step,
-        'B-step': B_step,
-}
+data = {'speed_input': speed_in,
+        'luminance_nput': lum_in,
+        'A_step': A_step,
+        'B_step': B_step,}
 
 df = pd.DataFrame(data)
 print(df.head())
 
 df.to_csv('rotation_dataset.csv', index=False)
+print("Dataset saved as 'rotation_dataset.csv'")
